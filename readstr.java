47336543a74f179878062696d37af07c49a0a843
Jayakrishnan M <jayakrishnan.m99@gmail.com>
2018-11-30 10:00:27 +0530

    Modified

---
+++ readstr.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class readstr{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		String s;
+		System.out.println("Enter a string to read:");
+		s=sc.nextLine();
+		System.out.println("The string you entered is:"+s);
+	}
+}
